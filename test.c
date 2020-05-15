1937ae203a3e21ed22e9f20c9532e73f1a6fda0f
ljf <ljf@qq.com>
2020-05-15 14:46:19 +0800

    test

---
+++ test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>//函数练习
+void printf_C();
+void printf_C()
+{
+
+	printf(" ###### \n");
+	printf("##    ##\n");
+	printf("##      \n");
+	printf("##      \n");
+	printf("##      \n");
+	printf("##    ##\n");
+	printf(" ###### \n");
+}
+int main()
+{
+	printf_C();
+	printf("\n");
+	printf_C();
+	printf("\n");
+	printf_C();
+	return 0;	
+
+
+}
